66790fa9c09cb69b5476c74e79fb4ebd178fba7c
valentin-lm <vava56620@hotmail.com>
2022-09-09 08:56:13 +0200

    Update import Sql et modification config.php

--- TutoInscription-master/config.php
+++ TutoInscription-master/config.php
@@ -8,7 +8,7 @@
          */
     try
     {
-        $bdd = new PDO("mysql:host=localhost;dbname=inscription;charset=utf8", "root", "");
+        $bdd = new PDO("mysql:host=localhost;dbname=ersd;charset=utf8", "root", "");
     }
     catch(PDOException $e)
     {
