cbd1554c971012077b88ec92cf501bf52ed802d6
valentin-lm <vava56620@hotmail.com>
2022-09-10 23:20:14 +0200

    ajout du systel d'insertion d'image

--- PopArTech02/inscription_traitement.php
+++ PopArTech02/inscription_traitement.php
@@ -2,11 +2,14 @@
     require_once 'config.php'; // On inclu la connexion à la bdd
 
     // Si les variables existent et qu'elles ne sont pas vides
-    if(!empty($_POST['pseudo']) && !empty($_POST['email']))
+    if(!empty($_POST['pseudo']) && !empty($_POST['email']) && !isset($_POST['btn-add']))
     {
         // Patch XSS
         $pseudo = htmlspecialchars($_POST['pseudo']);
         $email = htmlspecialchars($_POST['email']);
+        $images=$_FILES['profile']['name'];
+        $tmp_dir=$_FILES['profile']['tmp_name'];
+        $imageSize=$_FILES['profile']['size'];
 
         // On vérifie si l'utilisateur existe
         $check = $bdd->prepare('SELECT pseudo, email FROM utilisateurs WHERE email = ?');
@@ -24,20 +27,28 @@
 
                             $ip = $_SERVER['REMOTE_ADDR'];
 
+                            $imgExt=strtolower(pathinfo($images,PATHINFO_EXTENSION));
+                            $valid_extensions=array('jpeg', 'jpg', 'png', 'gif', 'pdf');
+                            $picProfile=rand(1000, 1000000).".".$imgExt;
+                            $upload_dir='./uploads/';
+
+                            move_uploaded_file($tmp_dir, $upload_dir.$picProfile);
+
+
                             // On insère dans la base de données
-                            $insert = $bdd->prepare('INSERT INTO utilisateurs(pseudo, email, ip, token) VALUES(:pseudo, :email, :ip, :token)');
+                            $insert = $bdd->prepare('INSERT INTO utilisateurs(pseudo, email, ip, token, img) VALUES(:pseudo, :email, :ip, :token, :img)');
                             $insert->execute(array(
                                 'pseudo' => $pseudo,
                                 'email' => $email,
                                 'ip' => $ip,
-                                'token' => bin2hex(openssl_random_pseudo_bytes(6))) // a enlever a mettre la description
+                                'token' => bin2hex(openssl_random_pseudo_bytes(6)), // a enlever a mettre la description
+                                'img' => $picProfile)
                             );
                             // On redirige avec le message de succès
-                            header('Refresh: 3; index.php?reg_err=success');
-                            die();
-
+                            header('Refresh: 0; index.php?reg_err=success');die();
                     }else{ header('Location: index.php?reg_err=email'); die();}
                 }else{ header('Location: index.php?reg_err=email_length'); die();}
             }else{ header('Location: index.php?reg_err=pseudo_length'); die();}
         }else{ header('Location: index.php?reg_err=already'); die();}
     }
+
